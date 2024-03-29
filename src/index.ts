368561e46c6c09d20feb9e613d01d37a83cabf24
Olivier Louvignes <olivier@mgcrea.io>
2024-03-29 22:08:41 +0100

    feat(update): minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 export * from "./client";
+
+import type { ProtoGrpcType } from "./types/grpc_service";
+
+export type ServerLiveRequest = ProtoGrpcType["inference"]["ServerLiveRequest"];
+export type ServerLiveResponse = ProtoGrpcType["inference"]["ServerLiveResponse"];
+export type ServerReadyRequest = ProtoGrpcType["inference"]["ServerReadyRequest"];
+export type ServerReadyResponse = ProtoGrpcType["inference"]["ServerReadyResponse"];
+export type RepositoryIndexRequest = ProtoGrpcType["inference"]["RepositoryIndexRequest"];
+export type RepositoryIndexResponse = ProtoGrpcType["inference"]["RepositoryIndexResponse"];
+export type ModelMetadataRequest = ProtoGrpcType["inference"]["ModelMetadataRequest"];
+export type ModelMetadataResponse = ProtoGrpcType["inference"]["ModelMetadataResponse"];
+export type ModelInferRequest = ProtoGrpcType["inference"]["ModelInferRequest"];
+export type ModelInferResponse = ProtoGrpcType["inference"]["ModelInferResponse"];
+
+export type { ProtoGrpcType };
