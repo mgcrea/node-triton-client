dc05c665c0ca3c08a3798a7f7cc1fa3d6e85765a
Olivier Louvignes <olivier@mgcrea.io>
2024-04-12 16:19:53 +0200

    feat(update): minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./client";
+export type { ProtoGrpcType } from "./types/grpc_service";
+export type * from "./types/inference";
 
 export { bufferToFloat32Array } from "./utils/buffer";
-
-export type { ProtoGrpcType } from "./types/grpc_service";
